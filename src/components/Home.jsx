3c48b99bcbef86ef526a1f6ae416bb6e708b7b10
danishr0307 <mddanishraza0007@gmail.com>
2022-08-16 22:39:08 +0530

    font icon change color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,63 +1,65 @@
-import React from 'react'
-import '../Home.css'
-import '../App.css'
-import { useState, useRef, useEffect } from 'react';
-import SearchResult from './SearchResult';
-import Receipe from './Receipe';
+import React from "react";
+import "../Home.css";
+import "../App.css";
+import { useState, useRef, useEffect } from "react";
+import SearchResult from "./SearchResult";
+import Receipe from "./Receipe";
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('')
+  const [keyword, setKeyword] = useState("");
   const [meals, setMeals] = useState([]);
-  const [currentReceipe, setCurrentReceipe] = useState(null)
-  const [showResult, setShowResult] = useState(false)
-  const [showReceipe, setShowReceipe] = useState(false)
-  const resultHeading = useRef()
+  const [currentReceipe, setCurrentReceipe] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [showReceipe, setShowReceipe] = useState(false);
+  const resultHeading = useRef();
   const search = async (e) => {
     e.preventDefault();
-    if (keyword === '') {
-      alert('Please enter a search terms.');
+    if (keyword === "") {
+      alert("Please enter a search terms.");
       return;
     }
     try {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + keyword)
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + keyword
+      );
       const data = await response.json();
       if (data.meals === null) {
         resultHeading.current.textContent = `There are no search results. Try again!`;
-        setShowResult(false)
-        setKeyword('')
+        setShowResult(false);
+        setKeyword("");
         return;
       } else {
         setMeals(data.meals);
-        setShowResult(true)
-        setShowReceipe(false)
-        setKeyword('')
+        setShowResult(true);
+        setShowReceipe(false);
+        setKeyword("");
         resultHeading.current.textContent = `Search result for '${keyword}':`;
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (showReceipe) {
-      resultHeading.current.textContent = ''
+      resultHeading.current.textContent = "";
     }
-  }, [showReceipe])
-
+  }, [showReceipe]);
 
   const random = async () => {
     try {
-      setMeals([])
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php' + keyword)
+      setMeals([]);
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php" + keyword
+      );
       const data = await response.json();
       setCurrentReceipe(data.meals[0]);
-      setShowReceipe(true)
-      setShowResult(false)
+      setShowReceipe(true);
+      setShowResult(false);
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
 
   return (
     <div className="container">
@@ -68,28 +70,26 @@ export default function Home() {
             type="text"
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
-            placeholder="Search for meals or keywords" />
+            placeholder="Search for meals or keywords"
+          />
 
           <button className="search-btn" type="submit">
-            <i className="fas fa-search" aria-hidden="true"></i>
+            <i class="fa fa-search icon" aria-hidden="true"></i>
           </button>
         </form>
         <button className="random-btn" id="random" onClick={random}>
-          <i className="fa fa-random" aria-hidden="true"></i>
+          <i class="fa fa-random icon" aria-hidden="true"></i>
         </button>
       </div>
       <div id="result-heading">
-        <h2 ref={resultHeading}>
-        </h2>
+        <h2 ref={resultHeading}></h2>
       </div>
-      {
-        showResult ? <>
+      {showResult ? (
+        <>
           <SearchResult meals={meals} />
-        </> : null
-      }
-      {
-        showReceipe ? <Receipe meal={currentReceipe} /> : null
-      }
+        </>
+      ) : null}
+      {showReceipe ? <Receipe meal={currentReceipe} /> : null}
     </div>
-  )
+  );
 }
